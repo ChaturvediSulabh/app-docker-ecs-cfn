a0c894b69c59f2d8ca9443f006d8a16876a95fac
ChaturvediSulabh <sulabhchaturvedi08@gmail.com>
2019-03-19 17:38:53 +0800

    Update pipeline

--- go-simple-webserver.go
+++ go-simple-webserver.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"net/http"
 	"fmt"
+	"net/http"
 )
 
-func main(){
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello There, How are you ?")
 	})
 	http.ListenAndServe(":80", nil)
